c3f3fd14f8aea1ea4923616c503359dc0fb65738
conchlin <connorbeckett@protonmail.com>
2023-04-17 19:04:40 -0400

    add clickable 'Start Game' button

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"image/color"
 	"log"
 	"os"
 
@@ -12,7 +11,8 @@ import (
 	"gioui.org/io/system"
 	"gioui.org/layout"
 	"gioui.org/op"
-	"gioui.org/text"
+	"gioui.org/unit"
+	"gioui.org/widget"
 	"gioui.org/widget/material"
 	_ "github.com/lib/pq"
 )
@@ -44,9 +44,12 @@ func initDatabase(host string, port string, user string, password string, dbname
 
 func initGUI() {
 	go func() {
-		w := app.NewWindow()
-		err := runGUI(w)
-		if err != nil {
+		// create new window
+		w := app.NewWindow(
+			app.Title("Scoober"),
+			app.Size(unit.Dp(800), unit.Dp(600)),
+		)
+		if err := draw(w); err != nil {
 			log.Fatal(err)
 		}
 		os.Exit(0)
@@ -54,24 +57,59 @@ func initGUI() {
 	app.Main()
 }
 
-func runGUI(w *app.Window) error {
-	th := material.NewTheme(gofont.Collection())
+type C = layout.Context
+type D = layout.Dimensions
+
+func draw(w *app.Window) error {
 	var ops op.Ops
-	for {
-		e := <-w.Events()
+	var startButton widget.Clickable
+	th := material.NewTheme(gofont.Collection())
+	var activeGame bool = false
+
+	// listen for events in the window.
+	for e := range w.Events() {
 		switch e := e.(type) {
-		case system.DestroyEvent:
-			return e.Err
+		// window rendering
 		case system.FrameEvent:
 			gtx := layout.NewContext(&ops, e)
-
-			title := material.H1(th, "Scoober")
-			maroon := color.NRGBA{R: 127, G: 0, B: 0, A: 255}
-			title.Color = maroon
-			title.Alignment = text.Middle
-			title.Layout(gtx)
-
+			if startButton.Clicked() {
+				activeGame = true
+				fmt.Println("Button has been clicked and game has started")
+			}
+			layout.Flex{
+				Axis:    layout.Vertical,
+				Spacing: layout.SpaceStart,
+			}.Layout(gtx,
+				layout.Rigid(
+					func(gtx C) D {
+						// define the margins for the start button
+						margins := layout.Inset{
+							Top:    unit.Dp(25),
+							Bottom: unit.Dp(25),
+							Right:  unit.Dp(35),
+							Left:   unit.Dp(35),
+						}
+						return margins.Layout(gtx,
+							// add the start button
+							func(gtx C) D {
+								var text string
+								if !activeGame {
+									text = "Start Game"
+								} else {
+									text = "Stop Game"
+								}
+								btn := material.Button(th, &startButton, text)
+								return btn.Layout(gtx)
+							},
+						)
+					},
+				),
+			)
 			e.Frame(gtx.Ops)
+		// window destroy
+		case system.DestroyEvent:
+			return e.Err
 		}
 	}
+	return nil
 }
